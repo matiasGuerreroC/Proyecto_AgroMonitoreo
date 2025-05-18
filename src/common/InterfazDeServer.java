e6797f554f7ff8079463cb24f620a1cb3a0f9549
fabipv <112329085+fabipv@users.noreply.github.com>
2025-05-18 17:30:04 -0400

    feat(common): add clienteConectado() method to InterfazDeServer.java

--- src/common/InterfazDeServer.java
+++ src/common/InterfazDeServer.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 
 public interface InterfazDeServer extends Remote {
     public ArrayList<ClimaCiudad> getHistorial() throws RemoteException;
-    ArrayList<String> obtenerHistorialAlertas(String ciudad) throws RemoteException;
+    public ArrayList<String> obtenerHistorialAlertas(String ciudad) throws RemoteException;
     public ClimaCiudad consultarClima(String ciudad) throws RemoteException;
     public ArrayList<String> generarAlertas(ClimaCiudad clima) throws RemoteException;
+
+    public void clienteConectado() throws RemoteException;
 }
