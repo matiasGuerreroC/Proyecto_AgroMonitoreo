7bc94d202f1add5fe1d8dd2ce4e388b5b370fee2
fabipv <112329085+fabipv@users.noreply.github.com>
2025-05-18 18:15:01 -0400

    feat(client): add clienteConectado() call in Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -17,6 +17,45 @@ public class Client {
         server = (InterfazDeServer) registry.lookup("server");
     }
 
+    public void clienteConectado() throws RemoteException {
+        server.clienteConectado();
+    }
+
+    public ArrayList<ClimaCiudad> getHistorial() throws RemoteException {
+        return server.getHistorial();
+    }
+
+    public ClimaCiudad consultarClima(String ciudad) throws RemoteException {
+        return server.consultarClima(ciudad);
+    }
+    
+    public ArrayList<String> generarAlertas(ClimaCiudad clima) throws RemoteException {
+        return new ArrayList<>(server.generarAlertas(clima));
+    }
+
+    public ArrayList<String> obtenerHistorialAlertas(String ciudad) throws RemoteException {
+        return new ArrayList<>(server.obtenerHistorialAlertas(ciudad));
+    }
+}
+package client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+
+import common.ClimaCiudad;
+import common.InterfazDeServer;
+
+public class Client {
+    private InterfazDeServer server;
+
+    public Client() throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry("localhost", 1009);
+        server = (InterfazDeServer) registry.lookup("server");
+    }
+
     public ArrayList<ClimaCiudad> getHistorial() throws RemoteException {
         return server.getHistorial();
     }
